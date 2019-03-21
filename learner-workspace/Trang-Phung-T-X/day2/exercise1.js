370a10dcbe1cbc0fb7d1716c87e8f8b0a3ee82d1
at-trangphung <trang.phung@asiantech.vn>
2019-03-21 10:12:39 +0700

    add solution

--- learner-workspace/Trang-Phung-T-X/day2/exercise1.js
+++ learner-workspace/Trang-Phung-T-X/day2/exercise1.js
@@ -10,7 +10,7 @@ var students = [
 function less24(students) {
   let a = [];
   for (let i = 0; i < students.length; i++) {
-    if (students[i]["age"] < 24) {
+    if (students[i]['age'] < 24) {
       a.push(students[i]);
     }
   }
@@ -19,69 +19,89 @@ function less24(students) {
 //solution2
 let newArrStudent = students.filter(student => student.age < 24);
 
+
+
 // 2.Tìm đến người có tên Huynh Thi B đổi tuổi thành 25
+//solution1
 function newStudents(students) {
   let i = 0;
   while (i < students.length) {
-    if (students[i]["name"] == 'Huynh Thi B') {
-      students[i]["age"] = 25;
+    if (students[i]['name'] === 'Huynh Thi B') {
+      students[i]['age'] = 25;
     }
     i++;
   }
   return students;
 }
+//solution2
+let newStudent = students.map(student => {
+  if (student.name === 'Huynh Thi B') {
+    return { name: student.name, age: 25 };
+  }
+  return student
+});
 
 
 // 3.Tính tổng tuổi của danh sách students trên
+//solution1
 function sumAgeStudent(students) {
   let sumAge = 0;
   for (let i = 0; i < students.length; i++) {
-    sumAge += students[i]["age"];
+    sumAge += students[i]['age'];
   }
   return sumAge;
 }
-
 //solution2
 let ageStudents = students.map(value => value.age);
 let total = ageStudents.reduce((sum, value) => sum + value, 0);
+// solution3
+console.log(ageStudents.reduce(function (accumulator, currentValue, currentIndex, array) {
+  return accumulator + currentValue;
+}));
+
 
 // 4.Tìm ra người có tuổi bằng 20
 //solution1
 function student20Age(students) {
   let i = 0;
   while (i < students.length) {
-    if (students[i]["age"] == 20) {
+    if (students[i]['age'] === 20) {
       return students[i];
     }
     i++;
   }
 }
 //solution2
-let student20 = students.find(student => student.age == 20);
+let student20 = students.find(student => student.age === 20);
+
+
 
 // 5.Tìm index của người có tuổi bằng 19
 //solution1
 function student19Age(students) {
   let i = 0;
   while (i < students.length) {
-    if (students[i]["age"] == 19) {
+    if (students[i]['age'] === 19) {
       return i;
     }
     i++;
   }
 }
 //solution2
-let student19 = students.findIndex(student => student.age == 19);
+let student19 = students.findIndex(student => student.age === 19);
 
 
 // 6.Tìm xem trong students có người nào là Le Van D không, trả về boolean
+//solution1
 function checkStudent(students) {
   let i = 0;
   while (i < students.length) {
-    if (students[i]["name"] == 'Le Van D') {
+    if (students[i]['name'] === 'Le Van D') {
       return true;
     }
     i++;
   }
   return false;
 }
+//solution2
+let checkStudent = students.some(student => student.name === 'Le Van D')
