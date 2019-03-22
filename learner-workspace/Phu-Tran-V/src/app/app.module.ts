dfc9c1bdc34a4902331ea29e2f5aa67b471ba263
phutran1 <justinphus@gmail.com>
2019-03-22 10:14:01 +0700

    ex3

---
+++ learner-workspace/Phu-Tran-V/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WordComponent } from './word.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    WordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
