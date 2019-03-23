e0bf1b972b2efc0190faad041f0f779db8510bd0
at-betruong <be.truong@asiantech.vn>
2019-03-23 21:50:08 +0700

    angular: exercise2-Input,Output

---
+++ learner-workspace/Be-Truong-T/exercise-angular/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { asTextData } from '@angular/core/src/view';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'erxercise-angular';
+
+  listTabTitle = ['Home', 'Profile', 'Contact'];
+  currentTab = this.listTabTitle[0];
+
+  clicked(event) {
+    console.log('Current tab clinking: ');
+    console.log(event.target.innerHTML);
+    this.currentTab = event.target.innerHTML;
+ }
+}
