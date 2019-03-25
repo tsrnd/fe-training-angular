6c9aa8309897fe374ba1c9e5b0c0e6f5d7d08353
at-hieutran2 <hieu.tran2@asiantech.vn>
2019-03-25 10:05:11 +0700

    Excercise 4: Angular day 2

--- learner-workspace/Hieu-Tran-M/my-app/src/app/app.component.ts
+++ learner-workspace/Hieu-Tran-M/my-app/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular training';
+  content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+  getContent(type: string): void {
+    if (type === 'home') {
+      this.content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+    } else if (type === 'profile') {
+      // tslint:disable-next-line:max-line-length
+      this.content = 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+    } else {
+      // tslint:disable-next-line:max-line-length
+      this.content = 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
+    }
+  }
 }
--- learner-workspace/Hieu-Tran-M/my-app/src/app/app.module.ts
+++ learner-workspace/Hieu-Tran-M/my-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomeComponent } from './features/home/home.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    HomeComponent,
+    ProfileComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
