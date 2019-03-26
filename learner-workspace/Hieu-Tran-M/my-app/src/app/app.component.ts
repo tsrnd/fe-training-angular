829011cadce0b9cf42f198f45c6449d4f6702cfe
at-hieutran2 <hieu.tran2@asiantech.vn>
2019-03-26 10:30:46 +0700

    Refactor code

--- learner-workspace/Hieu-Tran-M/my-app/src/app/app.component.ts
+++ learner-workspace/Hieu-Tran-M/my-app/src/app/app.component.ts
@@ -6,20 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
-  showTab = [true, false, false];
+  indexTab = 0;
   getContent(type: string): void {
     if (type === 'home') {
-      this.content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
-      this.showTab = [true, false, false];
+      this.indexTab = 0;
     } else if (type === 'profile') {
-      // tslint:disable-next-line:max-line-length
-      this.content = 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
-      this.showTab = [false, true, false];
+      this.indexTab = 1;
     } else {
-      // tslint:disable-next-line:max-line-length
-      this.content = 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
-      this.showTab = [false, false, true];
+      this.indexTab = 2;
     }
   }
 }
