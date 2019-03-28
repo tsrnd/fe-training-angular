a5cc246ac80a2a2358b72947c86a9f86038a5788
ha-phan <ha.phan@asiantech.vn>
2019-03-29 01:03:25 +0700

    fix ex6

--- learner-workspace/Ha-Phan-T/my-app/src/app/lifecycle-title/lifecycle-title.component.ts
+++ learner-workspace/Ha-Phan-T/my-app/src/app/lifecycle-title/lifecycle-title.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Output, EventEmitter, ContentChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { LifecycleContentComponent } from './../lifecycle-content/lifecycle-content.component';
+import { ModalComponent } from './../modal/modal.component';
 
 @Component({
   selector: 'app-lifecycle-title',
@@ -11,6 +12,8 @@ export class LifecycleTitleComponent implements OnInit {
   @Input() method: any;
   @Output() itemDelete = new EventEmitter();
   @ViewChild(LifecycleContentComponent) contentComponent: LifecycleContentComponent;
+  @ContentChild(ModalComponent) modal: ModalComponent;
+
   constructor() { }
 
   ngOnInit() {
