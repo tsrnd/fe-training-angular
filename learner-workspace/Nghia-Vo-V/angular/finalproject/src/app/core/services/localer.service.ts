5588f2b89a57ef3065f71575d1c4d63ebeaae4f1
Nghia Vo V <nghia.vo@asiantech.vn>
2019-05-01 14:03:37 +0700

    final project

---
+++ learner-workspace/Nghia-Vo-V/angular/finalproject/src/app/core/services/localer.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalerService {
+
+  constructor() { }
+
+  saveLocalStorage(key: string, value: any) {
+    value = (typeof value === 'object') ? JSON.stringify(value) : value;
+    return localStorage.setItem(key, value);
+  }
+
+  getLocalStorage(key: string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  saveSessionStorage(key: string, value: any) {
+    value = (typeof value === 'object') ? JSON.stringify(value) : value;
+    return sessionStorage.setItem(key, value);
+  }
+
+  getSessionStorage(key: string) {
+    return sessionStorage.getItem(key);
+  }
+
+  removeLocalStorage(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  clearLocalStorage() {
+    localStorage.clear();
+  }
+}
--- learner-workspace/Nghia-Vo-V/angular/finalproject/src/app/features/products/product-list/product-list.component.ts
+++ learner-workspace/Nghia-Vo-V/angular/finalproject/src/app/features/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-  
-  constructor() { }
+  url: any;
+  dataProducts = [];
+  dataCat = [];
+  type = [
+    {
+      id: 1,
+      name: "women",
+      url: "/women"
+    },
+    {
+      id: 2,
+      name: "men",
+      url: "/men"
+    },
+    {
+      id: 3,
+      name: "kids",
+      url: "/kids"
+    },
+  ]
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+  ) {
+
+  }
 
   ngOnInit() {
-    
+    this.url = this.router.url;
+    if (this.url == this.type[0].url) {
+      this.apiService.get("categories/" + this.type[0].url).subscribe(datas => this.dataCat = datas);
+      this.apiService.get("categories/" + this.type[0].url + "/products").subscribe(datas => this.dataProducts = datas);
+    }
+    if (this.url == this.type[1].url) {
+      this.apiService.get("categories/" + this.type[1].url).subscribe(datas => this.dataCat = datas);
+      this.apiService.get("categories/" + this.type[1].url + "/products").subscribe(datas => this.dataProducts = datas);
+    }
+    if (this.url == this.type[2].url) {
+      this.apiService.get("categories/" + this.type[2].url).subscribe(datas => this.dataCat = datas);
+      this.apiService.get("categories/" + this.type[2].url + "/products").subscribe(datas => this.dataProducts = datas);
+    }
   }
+  
 }
