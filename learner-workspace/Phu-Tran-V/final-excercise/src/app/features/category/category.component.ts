b3ef907c309512273670781541939ee05da1fbb8
phutran1 <justinphus@gmail.com>
2019-05-03 15:02:03 +0700

    update code

--- learner-workspace/Phu-Tran-V/final-excercise/src/app/features/category/category.component.ts
+++ learner-workspace/Phu-Tran-V/final-excercise/src/app/features/category/category.component.ts
@@ -8,12 +8,17 @@ import { ApiService } from '../../core/service/api.service';
 })
 export class CategoryComponent implements OnInit {
   woman;
+  // man;
+  // kid;
   constructor(
     private api: ApiService
   ) { }
 
   ngOnInit() {
     this.api.get('category/1/product?p=1&l=4').subscribe(data => this.woman = data);
+    console.log(this.woman)
+    // this.api.get('category/1/product?p=1&l=4').subscribe(data => this.man = data);
+    // this.api.get('category/1/product?p=1&l=4').subscribe(data => this.kid = data);
   }
 
 }
