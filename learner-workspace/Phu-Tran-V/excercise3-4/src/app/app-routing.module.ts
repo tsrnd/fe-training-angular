a90e33da842004751b1a2ea65de1a870a34c91d8
phutran1 <justinphus@gmail.com>
2019-05-09 16:42:06 +0700

    update header for register page

--- learner-workspace/Phu-Tran-V/excercise3-4/src/app/app-routing.module.ts
+++ learner-workspace/Phu-Tran-V/excercise3-4/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FeaturesComponent } from './features/features.component';
-
 const routes: Routes = [
   {
     path: '',
--- learner-workspace/Phu-Tran-V/excercise3-4/src/app/features/features.module.ts
+++ learner-workspace/Phu-Tran-V/excercise3-4/src/app/features/features.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
-// import { FeaturesComponent } from './features.component';
+import { FeaturesComponent } from './features.component';
+import { CategoryComponent } from '../../../../final-excercise/src/app/features/category/category.component';
 
 @NgModule({
-  // declarations: [FeaturesComponent],
+  declarations: [FeaturesComponent, CategoryComponent],
   imports: [
     CommonModule,
     FeaturesRoutingModule
